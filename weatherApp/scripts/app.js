2b20353bcb91217ad81f8060d8ac91c8159f8f0e
Olatunji Temitayo <103120095+olatemitayo@users.noreply.github.com>
2022-11-18 08:43:07 +0100

    Add files via upload

---
+++ weatherApp/scripts/app.js
@@ -0,0 +1,45 @@
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+
+const updateUI = (data) => {
+  //   const cityDets = data.cityDets;
+  //   const weather = data.weather;
+
+  const { cityDets, weather } = data;
+  //update details templates
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.LocalizedName}</h5>
+    <div class="my-3">${weather.Temperature.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>
+  `;
+
+  //remove the d-none class if present
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
+
+const updateCity = async (city) => {
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets);
+
+  return { cityDets, weather };
+};
+
+cityForm.addEventListener("submit", (e) => {
+  //prevent default action
+  e.preventDefault();
+
+  //get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  //update the ui with new city
+  updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+});
